a19c5dcc2dc117e13b8ae62c6e4a4547bcccf369
Elif Keten <elifbeyzaketen@gmail.com>
2023-01-26 10:37:30 +0100

    Init

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    
+	 int a, b, c;
+	 
+	 a = 5;
+	 b = 10;
+	 c = a + b;
+	 
+    System.out.println("a = "+ a + " b = " + b + " c = " + c);
+	 
+		
+		
+		
+	}
+}
